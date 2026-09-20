23c8301eb5eaa3c7f0e2d2d26308735a57e4f57a
patricia <patriciamoraissilvaaa@gmail.com>
2026-09-20 16:48:21 -0300

    Adiciona códigos para análise

--- codigos/code llama/8-ControleEstoque.py
+++ codigos/code llama/8-ControleEstoque.py
@@ -46,6 +46,12 @@ class Estoque:
     def consultar_produtos_por_categoria(self, categoria):
         return [produto for produto in self.produtos if produto.categoria == categoria]
 
+    def verificar(valor):
+        if valor == True:
+            return True
+        else:
+            return False
+
     def gerar_relatorio(self):
         relatorio = []
         valor_total_estimado = 0
